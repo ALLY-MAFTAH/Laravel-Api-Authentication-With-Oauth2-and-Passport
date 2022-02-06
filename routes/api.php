2b4d365eef39986c669fd3888d9d3bf54b29f247
Maftah <ally.maftah@tangent-technologies.net>
2022-02-06 22:56:59 +0300

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['prefix' => 'user/oauth2'], function () {
+
+    Route::post('login',  [App\Http\Controllers\AuthController::class, 'login']);
+
+    Route::post('signup', [App\Http\Controllers\AuthController::class,'signup']);
+
+    Route::group(['middleware' => 'auth:api'], function () {
+
+        Route::get('logout', [App\Http\Controllers\AuthController::class,'logout']);
+
+        Route::get('user', [App\Http\Controllers\AuthController::class,'user']);
+    });
+});
+
+
+
+// ****** WHAT I FOUND
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
